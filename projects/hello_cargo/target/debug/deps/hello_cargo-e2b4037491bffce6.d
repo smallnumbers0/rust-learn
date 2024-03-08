afdb691ebf686a542fb271ee8d480e82cb72a6ff
Jacky <jchoi1459@gmail.com>
2024-03-08 14:13:54 -0800

    add cargo lesson

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d
@@ -0,0 +1,5 @@
+/Users/jackychoi/rust-learn/projects/hello_cargo/target/debug/deps/libhello_cargo-e2b4037491bffce6.rmeta: src/main.rs
+
+/Users/jackychoi/rust-learn/projects/hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jackychoi/rust-learn/projects/hello_cargo/target/debug/hello_cargo: /Users/jackychoi/rust-learn/projects/hello_cargo/src/main.rs
