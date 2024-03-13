2a069cd3db4b8704632d83eb2fe69b4749001d36
Jacky <jchoi1459@gmail.com>
2024-03-13 12:09:45 -0700

    add ownership example

---
+++ projects/ownership/target/debug/deps/ownership-5ed9b633563ad85b.d
@@ -0,0 +1,5 @@
+/Users/jackychoi/rust-learn/projects/ownership/target/debug/deps/libownership-5ed9b633563ad85b.rmeta: src/main.rs
+
+/Users/jackychoi/rust-learn/projects/ownership/target/debug/deps/ownership-5ed9b633563ad85b.d: src/main.rs
+
+src/main.rs:
---
+++ projects/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/jackychoi/rust-learn/projects/ownership/target/debug/ownership: /Users/jackychoi/rust-learn/projects/ownership/src/main.rs
