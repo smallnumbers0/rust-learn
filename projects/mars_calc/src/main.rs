9af3d073faed6a19c5a14f85df7543f62e146d31
jacky <jchoi1459@gmail.com>
2024-03-25 19:37:24 -0700

    update mars calc

--- projects/mars_calc/src/main.rs
+++ projects/mars_calc/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    println!("Enter your weight (kg): ");
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight: f32 = calculate_weight_on_mars(weight); //mut is needed for a variable to be mutable
+    println!("Weight on Mars: {}kg", mars_weight); //macros are used for metaprogramming
+  
 }
+
+
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
+}
+
